2addefad50248b390b58449c467b689462c00b34
lanceplarsen <lancep.larsen43@gmail.com>
2018-01-31 10:28:00 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-data "terraform_remote_state" "k8s_cluster" {
-  backend = "atlas"
-  config {
-    name = "lanceplarsenv2/vault-webinar-demo-k8s-cluster"
-  }
-}
-
 provider "kubernetes" {
   host = "${terraform_remote_state.k8s_cluster.k8s_endpoint}"
   client_certificate = "${base64decode(terraform_remote_state.k8s_cluster.k8s_master_auth_client_certificate)}"
   client_key = "${base64decode(terraform_remote_state.k8s_cluster.k8s_master_auth_client_key)}"
   cluster_ca_certificate = "${base64decode(terraform_remote_state.k8s_cluster.k8s_master_auth_cluster_ca_certificate)}"
+}
+
+data "terraform_remote_state" "k8s_cluster" {
+  backend = "atlas"
+  config {
+    name = "lanceplarsenv2/vault-webinar-demo-k8s-cluster"
+  }
 }
 
 resource "kubernetes_service_account" "spring" {
