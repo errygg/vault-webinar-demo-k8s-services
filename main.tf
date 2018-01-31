275fdc95a2246795d8bd27017b7ee5c3f812655b
Lance Larsen <lance@hashicorp.com>
2018-01-31 09:04:16 -0800

    adding tf files

---
+++ main.tf
@@ -0,0 +1,92 @@
+data "terraform_remote_state" "k8s_cluster" {
+  backend = "atlas"
+  config {
+    name = "lanceplarsenv2/vault-webinar-demo-k8s-cluster"
+  }
+}
+
+provider "kubernetes" {
+  host = "${terraform_remote_state.k8cluster.k8s_endpoint}"
+  client_certificate = "${base64decode(terraform_remote_state.k8cluster.k8s_master_auth_client_certificate)}"
+  client_key = "${base64decode(terraform_remote_state.k8cluster.k8s_master_auth_client_key)}"
+  cluster_ca_certificate = "${base64decode(terraform_remote_state.k8cluster.k8s_master_auth_cluster_ca_certificate)}"
+}
+
+resource "kubernetes_service_account" "spring" {
+  metadata {
+    name = "spring"
+  }
+}
+
+resource "kubernetes_pod" "spring-backend" {
+  metadata {
+    name = "spring-backend"
+    labels {
+      App = "spring-backend"
+    }
+  }
+  spec {
+    service_account_name = "${kubernetes_service_account.spring.metadata.0.name}"
+    container {
+      image = "lanceplarsen/spring-vault-demo"
+      image_pull_policy = "Always"
+      name  = "spring-backend"
+      port {
+        container_port = 8080
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "spring-backend" {
+  metadata {
+    name = "spring-backend"
+  }
+  spec {
+    selector {
+      App = "${kubernetes_pod.spring-backend.metadata.0.labels.App}"
+    }
+    port {
+      port = 8080
+      target_port = 8080
+    }
+  }
+}
+
+resource "kubernetes_pod" "spring-frontend" {
+  metadata {
+    name = "spring-frontend"
+    labels {
+      App = "spring-frontend"
+    }
+  }
+  spec {
+    service_account_name = "${kubernetes_service_account.spring.metadata.0.name}"
+    container {
+      image = "rberlind/spring-frontend:k8s-auth"
+      image_pull_policy = "Always"
+      name  = "spring-frontend"
+      port {
+        container_port = 80
+      }
+    }
+  }
+
+  depends_on = ["kubernetes_service.spring-backend"]
+}
+
+resource "kubernetes_service" "spring-frontend" {
+  metadata {
+    name = "spring-frontend"
+  }
+  spec {
+    selector {
+      App = "${kubernetes_pod.spring-frontend.metadata.0.labels.App}"
+    }
+    port {
+      port = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default = "us-east1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-b (which must be in gcp_region)"
+  default = "us-east1-b"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "cluster_name" {
+  description = "Name of the K8s cluster"
+}
+
+variable "initial_node_count" {
+  description = "Number of worker VMs to initially create"
+  default = 1
+}
+
+variable "master_username" {
+  description = "Username for accessing the Kubernetes master endpoint"
+}
+
+variable "master_password" {
+  description = "Password for accessing the Kubernetes master endpoint"
+}
+
+variable "node_machine_type" {
+  description = "GCE machine type"
+  default = "n1-standard-1"
+}
+
+variable "node_disk_size" {
+  description = "Node disk size in GB"
+  default = "10"
+}
+
+variable "environment" {
+  description = "value passed to ACS Environment tag"
+  default = "dev"
+}
