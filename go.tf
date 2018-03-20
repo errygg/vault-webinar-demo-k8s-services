97733baeb61bf548cb4d33620bd14546e506a6ee
Lance Larsen <lance@hashicorp.com>
2018-03-19 18:37:02 -0700

    splitting apps

---
+++ go.tf
@@ -0,0 +1,87 @@
+resource "kubernetes_replication_controller" "go-frontend" {
+  metadata {
+    name = "go-frontend"
+    labels {
+      App = "go-frontend"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      App = "go-frontend"
+    }
+    template {
+    service_account_name = "${kubernetes_service_account.go.metadata.0.name}"
+    container {
+        image = "lanceplarsen/go-vault-demo"
+        image_pull_policy = "Always"
+        name = "go"
+        volume_mount {
+            mount_path = "/var/run/secrets/kubernetes.io/serviceaccount"
+            name = "${kubernetes_service_account.go.default_secret_name}"
+            read_only = true
+        }
+        volume_mount {
+            mount_path = "/app/config.toml"
+            sub_path = "config.toml"
+            name = "${kubernetes_config_map.go.metadata.0.name}"
+        }
+        port {
+            container_port = 3000
+        }
+    }
+    volume {
+        name = "${kubernetes_service_account.go.default_secret_name}"
+        secret {
+            secret_name = "${kubernetes_service_account.go.default_secret_name}"
+        }
+    }
+    volume {
+        name = "${kubernetes_config_map.go.metadata.0.name}"
+        config_map {
+            name = "go"
+            items {
+                key = "config"
+                path =  "config.toml"
+            }
+        }
+    }
+    }
+  }
+}
+
+resource "kubernetes_service" "go-frontend" {
+    metadata {
+        name = "go-frontend"
+    }
+    spec {
+        selector {
+            App = "${kubernetes_replication_controller.go-frontend.metadata.0.labels.App}"
+        }
+        port {
+            port = 3000
+            target_port = 3000
+        }
+        type = "LoadBalancer"
+    }
+}
+
+resource "kubernetes_config_map" "go" {
+  metadata {
+    name = "go"
+  }
+  data {
+    config = <<EOF
+    [database]
+    server="llarsenvaultdb.cihgglcplvpp.us-east-1.rds.amazonaws.com:5432"
+    name="postgres"
+    role="database/creds/order"
+    [vault]
+    server="http://34.200.226.105:8200"
+    authentication="kubernetes"
+    role="order"
+    service-account-token-file="/var/run/secrets/kubernetes.io/serviceaccount"
+EOF
+  }
+}
--- main.tf
+++ main.tf
@@ -18,105 +18,14 @@ resource "kubernetes_service_account" "spring" {
     }
 }
 
+resource "kubernetes_service_account" "go" {
+    metadata {
+        name = "go"
+    }
+}
+
 resource "kubernetes_service_account" "vault" {
     metadata {
         name = "vault"
     }
-}
-
-
-resource "kubernetes_replication_controller" "spring-frontend" {
-  metadata {
-    name = "spring-frontend"
-    labels {
-      App = "spring-frontend"
-    }
-  }
-
-  spec {
-    replicas = 10
-    selector {
-      App = "spring-frontend"
-    }
-    template {
-    service_account_name = "${kubernetes_service_account.spring.metadata.0.name}"
-    container {
-        image = "lanceplarsen/spring-vault-demo"
-        image_pull_policy = "Always"
-        name = "spring"
-        volume_mount {
-            mount_path = "/var/run/secrets/kubernetes.io/serviceaccount"
-            name = "${kubernetes_service_account.spring.default_secret_name}"
-            read_only = true
-        }
-        volume_mount {
-            mount_path = "/bootstrap.yaml"
-            sub_path = "bootstrap.yaml"
-            name = "${kubernetes_config_map.spring.metadata.0.name}"
-        }
-        port {
-            container_port = 8080
-        }
-    }
-    volume {
-        name = "${kubernetes_service_account.spring.default_secret_name}"
-        secret {
-            secret_name = "${kubernetes_service_account.spring.default_secret_name}"
-        }
-    }
-    volume {
-        name = "${kubernetes_config_map.spring.metadata.0.name}"
-        config_map {
-            name = "spring"
-            items {
-                key = "config"
-                path =  "bootstrap.yaml"
-            }
-        }
-    }
-    }
-  }
-}
-
-resource "kubernetes_service" "spring-frontend" {
-    metadata {
-        name = "spring-frontend"
-    }
-    spec {
-        selector {
-            App = "${kubernetes_replication_controller.spring-frontend.metadata.0.labels.App}"
-        }
-        port {
-            port = 8080
-            target_port = 8080
-        }
-        type = "LoadBalancer"
-    }
-}
-
-resource "kubernetes_config_map" "spring" {
-  metadata {
-    name = "spring"
-  }
-  data {
-    config = <<EOF
----
-spring.cloud.vault:
-    authentication: KUBERNETES
-    kubernetes:
-        role: order
-        service-account-token-file: /var/run/secrets/kubernetes.io/serviceaccount/token
-    host: 34.200.226.105
-    port: 8200
-    scheme: http
-    fail-fast: true
-    config.lifecycle.enabled: true
-    database:
-        enabled: true
-        role: order
-        backend: database
-spring.datasource:
-    url: jdbc:postgresql://llarsenvaultdb.cihgglcplvpp.us-east-1.rds.amazonaws.com:5432/postgres
-EOF
-  }
 }
