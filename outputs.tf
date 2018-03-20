6b30df22df1710f2cc4dd5431ad0cdcd646e81d2
Lance Larsen <lance@hashicorp.com>
2018-03-19 18:40:57 -0700

    adding go lb output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "spring_ip" {
   value = "${kubernetes_service.spring-frontend.load_balancer_ingress.0.ip}"
 }
+
+output "go_ip" {
+  value = "${kubernetes_service.gofrontend.load_balancer_ingress.0.ip}"
+}
