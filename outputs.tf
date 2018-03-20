12b44519b33a70db371ee6953a6c40fe3eb6e56f
lanceplarsen <lancep.larsen43@gmail.com>
2018-03-19 22:45:18 -0700

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "spring_ip" {
-  value = "${kubernetes_service.spring-frontend.load_balancer_ingress.0.ip}"
-}
-
-output "go_ip" {
-  value = "${kubernetes_service.go-frontend.load_balancer_ingress.0.ip}"
-}
