0b21ee6e31f4a650644315484ad5bb6dd23ab56e
lanceplarsen <lancep.larsen43@gmail.com>
2018-03-19 21:43:28 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "master_hostname" {
   description = "Hostname for accessing the Kubernetes master endpoint"
 }
 
-variable "master_username" {
-  description = "Username for accessing the Kubernetes master endpoint"
-}
-
-variable "master_password" {
-  description = "Password for accessing the Kubernetes master endpoint"
+variable "token" {
+  description = "token for accessing the Kubernetes master endpoint"
 }
