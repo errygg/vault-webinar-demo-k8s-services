65d996beddd7ac8be1cd55b0c63c93cf0f1976ed
Lance Larsen <lancep.larsen43@gmail.com>
2018-06-29 14:47:39 +1000

    Update go.tf

--- go.tf
+++ go.tf
@@ -7,7 +7,7 @@ resource "kubernetes_replication_controller" "go-frontend" {
   }
 
   spec {
-    replicas = 0
+    replicas = ${var.instance_count}
     selector {
       App = "go-frontend"
     }
